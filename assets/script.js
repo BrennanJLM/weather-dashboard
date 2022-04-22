179f6655bc63d5b7a3419438f89996e9be880566
Brennan <merrett.b@gmail.com>
2022-04-22 10:34:36 -0400

    api now displays current weather info

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city-search");
-var currentWeather = document.querySelector("#current-city-weath")
+var currentCityName = document.querySelector("#current-city-name")
+var currentCityTemp = document.querySelector("#current-city-temp")
+var currentCityFeelslike = document.querySelector("#current-city-feelslike")
+var currentCityWind = document.querySelector("#current-city-wind")
+var currentCityHumidity = document.querySelector("#current-city-humidity")
+var currentCityUVIndex = document.querySelector("#current-city-uvindex")
 var submitButton = document.querySelector("#submit-button")
 
 
@@ -12,9 +17,8 @@ var formSubmitHandler = function () {
 
     if (cityname) {
         // clear old content
-        currentWeather.textContent = "";
-
-        getCityWeather();
+        //currentWeather.textContent = "";
+        getCityWeather(cityInputEl.value);
 
     } else {
         //alert user
@@ -23,30 +27,21 @@ var formSubmitHandler = function () {
 
 }
 //get latitude/longitude     
-var getCityWeather = function () {
-
+var getCityWeather = async function(location) {
     var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=1&appid=d605dacbb35ec2fe801548d6b8c17a0d";
-
     fetch(apiUrl)
-        .then(function (response) {
-
-            if (response.ok) {
-                console.log(response)
-                return response.json()
-            }
-
+        .then(response => response.json())
+		.then(data => {
+			//console.log('DATA: ' + data)
+			currentCityName.innerText = data[0].name;
+			displayWeather(data[0].lat, data[0].lon);
         })
-        .then(function (data) {
-            console.log(data)
-
-            var latitude = data[0].lat;
-            var longitude = data[0].lon;
-
-            displayWeather(latitude,longitude, location);
-
-
-        })
-}
+		.catch(function(err) {
+			console.log(err);
+			return false;
+		});
+		
+};
 submitButton.addEventListener("click", function (event) {
     event.preventDefault();
     formSubmitHandler();
@@ -55,25 +50,51 @@ submitButton.addEventListener("click", function (event) {
 
 
 //display weather
-var displayWeather = function (latitude, longitude, location) {
-    
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=metric&appid=d605dacbb35ec2fe801548d6b8c17a0d";
-    
+var displayWeather = async function(latitude, longitude) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=d605dacbb35ec2fe801548d6b8c17a0d";
     fetch(apiUrl)
-      .then(function (response) {
-
-        if (response.ok) {
-            console.log(response)
-            return response.json()
-        
-            .then(function (data) {
-            displayWeather(data)
-            })
-
-       
-
-    }})
-}
+        .then(response => response.json())
+		.then(data => {
+			
+			//variables here
+			
+			
+			//console.log('DATA: ' + data)
+			currentCityTemp.innerText = data.current.temp;
+			currentCityFeelslike.innerText = data.current.feels_like;
+			currentCityWind.innerText = data.current.wind_speed;
+			currentCityHumidity.innerText = data.current.humidity;
+			currentCityUVIndex.innerText = data.current.uvi;
+			if(data.current.uvi >= 1) {
+				currentCityUVIndex.setAttribute('class', 'bg-danger text-white');
+			} else if(data.current.uvi > 0) {
+				currentCityUVIndex.setAttribute('class', 'bg-warning text-white');
+			} else {
+				currentCityUVIndex.setAttribute('class', 'bg-success text-white');
+			}
+			
+			//5day here
+			let card1 = document.createElement('li');
+			card1.setAttribute('class', 'card d-inline-block');
+			//<li class="card d-inline-block"><h4 class="card-header">Day 2</h4></li>
+			
+			document.querySelector('#current-city-5day ul').appendChild(card1);
+            date.textContent=moment.unix(data.daily[i].dt).format("L");
+            icon.innerHTML="<img src='http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png'>";
+            temp.textContent= "Temp: " + Math.round(data.daily[i].temp.day) + " °C";
+            wind.textContent= "Wind: " + mph(data.daily[i].wind_speed) + " KPH";
+            humidity.textContent= "Humidity: " + data.daily[i].humidity + "%";
+			document.querySelector('#current-city-5day ul').appendChild(card1.cloneNode(true));
+			document.querySelector('#current-city-5day ul').appendChild(card1.cloneNode(true));
+			document.querySelector('#current-city-5day ul').appendChild(card1.cloneNode(true));
+			document.querySelector('#current-city-5day ul').appendChild(card1.cloneNode(true));
+        })
+		.catch(function(err) {
+			console.log(err);
+			return false;
+		});
+		
+};
     
 
 
